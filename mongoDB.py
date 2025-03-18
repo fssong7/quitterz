bafbc46c68862edd5d348ecdd1403a9d123cf07e
fssong7 <48611530+fssong7@users.noreply.github.com>
2025-03-18 01:43:42 -0400

    Update mongoDB.py

--- mongoDB.py
+++ mongoDB.py
@@ -4,8 +4,8 @@ import os
 
 class database:
     def __init__(self):
-        #self.uri = os.environ.get("MONGO_URI")
-        self.uri = "mongodb+srv://fssong7:GE0SsNARhMKigDzv@cluster0.wqhp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        self.uri = os.environ.get("MONGO_URI")
+        #self.uri = "mongodb+srv://fssong7:GE0SsNARhMKigDzv@cluster0.wqhp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
         self.client = MongoClient(self.uri, server_api=ServerApi('1'))
         self.db = self.client["mydatabase"]
         self.collection = self.db["mycollection"]

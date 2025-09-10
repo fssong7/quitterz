added distribution chart to profiles, & sum of distributions to compare.  make compare - all time plot a moving average per week.  fixed bug in compare today's entries section

pages/compare.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 
+
 from inputs import people
 from statCalculator import dataAnalyzer
 
@@ -18,15 +19,12 @@ layout = html.Div([
     html.Div(style={'padding': '20px'}),
     html.H4("today's entries",style={'textAlign':'center'}),
     html.Div([
-        html.Div([
-            html.Div(id=f'{person["name_first"]} info', style={'textAlign': 'center'}),
-            html.Div(style={'padding': '5px'}),
-        ]) for person in people
+        html.Div(id=f'info-comp', style={'textAlign': 'center'})
     ]),
 
 
-    html.Div(id='saddest',style={'textAlign':'center'}),
-    dcc.Location(id='url', refresh=False),
+    html.Div(id='saddest-comp',style={'textAlign':'center'}),
+    dcc.Location(id='url-comp', refresh=False),
     html.Div(style={'padding': '20px'}),
     dcc.Tabs([
         dcc.Tab(label='last 7 days', children=[
@@ -42,13 +40,18 @@ layout = html.Div([
             dcc.Interval(id='interval-3', interval=10000, n_intervals=0)
         ])
     ]),
+    html.Div(style={'padding': '20px'}),
+    html.Div([
+        dcc.Graph(id="curve-comp"),
+        dcc.Interval(id=f"""interval-4""", interval=10000, n_intervals=0)
+    ])
 
 ])
 
 
 @callback(
-    Output('info','children'),
-    Input('url','href'),
+    Output('info-comp','children'),
+    Input('url-comp','href'),
     allow_duplicate=True
 )
 def rating(href):
@@ -70,14 +73,14 @@ def rating(href):
     return html.Div([html.P(msg) for msg in messages])
     
 @callback(
-    Output('saddest','children'),
-    Input('url','href'),
+    Output('saddest-comp','children'),
+    Input('url-comp','href'),
     allow_duplicate=True
 )
 def sad_compare(href):
     print("running sad_compare on compar")
+    
     vals = {}
-
     for name, analyzer in compare.items():
         df = analyzer.db[name]  
         index = analyzer.todays_entry(df)
@@ -86,22 +89,19 @@ def sad_compare(href):
         except (KeyError, IndexError):
             dval = None
         vals[name] = dval
-
-        submitted = {name: val for name, val in vals.items() if val is not None}
-
+    submitted = {name: val for name, val in vals.items() if val is not None}
     if not submitted:
-        return "nobody had submitted their ratings for today"
-    
+        return "Nobody had submitted their ratings for today."
     lowest_val = min(submitted.values())
     saddest = [name for name, val in submitted.items() if val == lowest_val]
-
     if len(saddest) == 1:
-        return f"good job {saddest[0]}, you are the saddest today!"
+        return f"Good job {saddest[0]}, you are the saddest today!"
+    elif len(saddest) == len(submitted) and len(submitted) == len(people):
+        return "Wow, we all had the same rating, we're like triplets!"
     elif len(saddest) == len(submitted):
-        return "wow we all had the same rating, we're like triplets"
-    else:
-        tied_names = ", ".join(saddest)
-        return f"Wow, there's a tie for saddest today: {tied_names}!"
+        tied_names = " & ".join(saddest)
+        return f"Wow, there's a tie for saddest today between {tied_names}!"
+        
     
 
 @callback(
@@ -182,19 +182,102 @@ def update_graph_3(n_intervals):
         for name, analyzer in compare.items():
             df = analyzer.db[name]
             recent_df,mean,std = analyzer.all_time(df)
-            fig.add_trace(go.Scatter(x=recent_df["date"], y=recent_df["dval"], mode='lines+markers', name=name))
-            titles.append(f"{name}: avg rating of {round(mean,2)} and std of {round(std,2)}")
 
-        fig.update_layout(title=f"quitterz throughout history<br>" + "<br>".join(titles),
-                        xaxis={'title': 'date'},
-                        yaxis={'title': 'depression level', 'range': [0, 11]},
-                        template="plotly_white",
-                        title_font_color="royalblue",
-                        title_x = 0.5,
-                        title_font=dict(size=14),
-                        legend=dict(
-                            yanchor="top",
-                            y = 0.5)
-                        )
+            recent_df['date'] = pd.to_datetime(recent_df['date'])
+            recent_df = recent_df.set_index('date')
+            weekly_df = recent_df['dval'].resample('W').mean()
+
+            moving_avg = weekly_df.rolling(window=1, center=True).mean()
+
+            fig.add_trace(go.Scatter(
+                x=weekly_df.index,
+                y=weekly_df.values,
+                mode='markers+lines',
+                name=f"{name} weekly avg",
+                line=dict(shape='spline', smoothing=1)
+            ))
+            # fig.add_trace(go.Scatter(
+            #     x=moving_avg.index,
+            #     y=moving_avg.values,
+            #     mode='lines',
+            #     name=f"{name} 4-week MA",
+            #     line=dict(dash='dash')
+            # ))
+
+            titles.append(f"{name}: avg rating of {round(mean,2)} and std of {round(std,2)}")
+            
+        #     fig.add_trace(go.Scatter(x=moving_avg.index, y=moving_avg.values, mode='lines+markers', name=name))
+        #     titles.append(f"{name}: avg rating of {round(mean,2)} and std of {round(std,2)}")
+
+        # fig.update_layout(title=f"quitterz throughout history<br>" + "<br>".join(titles),
+        #                 xaxis={'title': 'date'},
+        #                 yaxis={'title': 'depression level', 'range': [0, 11]},
+        #                 template="plotly_white",
+        #                 title_font_color="royalblue",
+        #                 title_x = 0.5,
+        #                 title_font=dict(size=14),
+        #                 legend=dict(
+        #                     yanchor="top",
+        #                     y = 0.5)
+        #                 )
         return fig
-    
+
+
+
+@callback(
+    Output("curve-comp", 'figure'),
+    Input(f"""interval-4""", 'n_intervals'),
+)
+def update_graph_4(n_intervals):
+        titles=[]
+        for analyzer in compare.values():
+            analyzer.update_db()
+
+        ratings = range(1, 11)
+        data = {}
+        
+        
+
+        for name, analyzer in compare.items():
+            df = analyzer.db[name]
+            recent_df, _, _ = analyzer.all_time(df)
+            counts = recent_df['dval'].value_counts().reindex(ratings, fill_value=0)
+            data[name] = counts
+        
+        df_counts = pd.DataFrame(data, index=ratings)
+        fig = go.Figure()
+
+        for i, name in enumerate(df_counts.columns):
+            fig.add_trace(go.Bar(
+                x=df_counts.index,
+                y=df_counts[name],
+                name=name,
+                
+            ))
+        
+        fig.add_trace(go.Scatter(
+            x=df_counts.index,
+            y=df_counts.sum(axis=1),
+            mode='lines+markers',
+            line=dict(shape='spline', smoothing=1.3, color="black")))
+
+
+        fig.update_layout(
+            title="distribution of depression over the entire history by quitter",
+            xaxis={'title': 'rating','range': [0, 11]},
+            yaxis={'title': 'count'},
+            template="plotly_white",
+            title_font_color="royalblue",
+            title_x=0.5,
+            legend=dict(
+                yanchor="top",
+                y = 0.5),
+            barmode="stack" #group
+        )
+            
+            
+
+
+        
+        return fig
+
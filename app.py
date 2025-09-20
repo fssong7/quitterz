43bb35ef4f02484bda6e8104f7c48fa7bc48c37f
sab18 <sarabarrows18@gmail.com>
2025-09-20 17:56:02 -0400

    final

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ app.layout = html.Div([
                 dash.page_container,
             ])
 
-if __name__ == '__main__':
-    app.run(debug=False, port=8063)
+# if __name__ == '__main__':
+app.run(debug=False, port=8063)
 
 
